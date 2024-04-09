0a5b035e44c41e496edf9c1c6a548fbb97c322d7
s1f10210368 <s1f102103684@iniad.org>
2024-04-09 21:08:37 +0900

    R

--- archive/clippy.rs
+++ archive/clippy.rs
@@ -1,5 +1,13 @@
 fn main() {
     let my_foo = "Foo";
+    for i in 0..3 {
+        // "{}"で第二引数の変数を出力
+        println!("{}", my_foo);
+    }
+}
+
+fn main() {
+    let my_foo = "Foo";
     for _i in 0..3 {
         // "{}"で第二引数の変数を出力
         println!("{}", my_foo);
