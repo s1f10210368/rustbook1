7dbd89197597612f28bc89877c2b8de1939b0db8
s1f10210368 <s1f102103684@iniad.org>
2024-04-19 18:05:53 +0900

    R

--- archive/kata.ts
+++ archive/kata.ts
@@ -12,3 +12,16 @@ let b : char = 'b';
 // 配列型
 let target = {"hoge", "fuga", "bar"};
 println!("{}", target[10]); //hoge
+
+// ベクター型
+let mut v: Vec<i32> = Vec::new(); // 空のベクターを作成
+v.push(99); // ベクターに要素を追加
+// "{:?}"で第二引数の変数をより詳細に出力
+println!("{:?}", v); // [99]
+
+// 文字列
+let message = "hello world";
+println!("{}", message); // hello world
+
+// ハッシュマップ
+// HashMap<K, V>はキー・バリュー形式で値を保持するコレクション
