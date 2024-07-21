3b864e46f5afebd0bd03d8415014bc3264269edc
s1f10210368 <s1f102103684@iniad.org>
2024-07-21 21:45:00 +0900

    R

--- function/enum.rs
+++ function/enum.rs
@@ -5,6 +5,16 @@ enum Color {
     Hex(String),
 }
 
-// 以下はどちらもColor型
+// 以下はどちらもColor型、enumは異なるデータ構造も含有することができる。
 let red = Color::Red;
 let hex = Color::Hex("ffffff".to_string());
+
+// Option、値があるかどうかを表すenum
+
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+// 上記では値を取り出すにはSomeかどうかの判別が必須になる。
+// この判別コードによりnull考慮漏れのようなバグの混入を防げる
